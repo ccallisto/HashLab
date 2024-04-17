cae98586d3a8aa971ab53baa394dbf602b9451df
ccallisto <gavinsingleton23@gmail.com>
2024-04-17 11:41:17 -0500

    moved some things

--- Hash.c
+++ Hash.c
@@ -3,16 +3,15 @@
 #include <stdlib.h>
 int main() {
   FILE *file;
-  char line[16];
-
+  char line[20]; //changed to 20 because of read errors
+  int result;
   file = fopen("input.txt", "r");
   while (fgets(line, sizeof(line), file)){
     printf("%s", line);
+    result = abs(( line[1] + ((line[3] + line[4] + line[6]  + line[7]) / 381 + line[0]) / 587 - line[10]));
+    printf("Hash result: %d\n", result);
   }
-  char str[] = "BCDEFGHIJKLMNOPQ";
   //burris values are shifted right by 1 here since my strings in C are 0..15
-     int result =abs( ( str[1] + ((str[3] + str[4] + str[6]  + str[7]) / 381 + str[0]) / 587 - str[10]));
-  printf("Hash result: %d\n", result);
   fclose(file);
     return 0;
 }
